495f6b4bda303b4ab9064efa29807d7f8903830d
Marine Leaute <leautemarine@yahoo.fr>
2018-08-09 23:13:39 +0200

    mise en forme

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,10 +1,11 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show]
+
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
@@ -16,15 +17,18 @@ class CocktailsController < ApplicationController
 
     if @cocktail.save!
       redirect_to cocktail_path(@cocktail)
+    else
+      render :new
     end
-
   end
 
   private
 
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
   def cocktail_params
     params.require(:cocktail).permit(:name)
   end
-
-
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,3 +1,37 @@
 class DosesController < ApplicationController
+  before_action :set_dose, only: [:destroy]
 
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredients = Ingredient.order(:name)
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id]) if params[:cocktail_id].present?
+    @dose.ingredient = Ingredient.find(params[:dose][:ingredient_id]) if params[:dose][:ingredient_id].present?
+    if @dose.save
+      redirect_to cocktail_path(params[:cocktail_id])
+    else
+      @cocktail = Cocktail.find(params[:cocktail_id])
+      @ingredients = Ingredient.order(:name)
+      render :new
+    end
+  end
+
+  def destroy
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
 end
