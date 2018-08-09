239941ddd9e5dd7e8b8e27461c9d246f0b701d5f
Marine Leaute <leautemarine@yahoo.fr>
2018-08-09 14:45:47 +0200

    validations & models

--- Gemfile
+++ Gemfile
@@ -56,7 +56,11 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-gem 'simple_form'
 
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+
+gem 'autoprefixer-rails'
+gem 'bootstrap-sass', '~> 3.3.7'
+gem 'font-awesome-sass', '~> 5.0.9'
+gem 'simple_form'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dose.destroy_all
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+lemon = Ingredient.create!(name: "lemon")
+ice = Ingredient.create!(name: "ice")
+mint_leave = Ingredient.create!(name: "mint leaves")
+
+surprise = Cocktail.create!(name: "Surprise")
+cosmo = Cocktail.create!(name: "Cosmopolitain")
+mojito = Cocktail.create!(name: "Mojito")
+
+Dose.create!(description: "6cl", cocktail: surprise, ingredient: lemon)
